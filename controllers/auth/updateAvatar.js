b429734cdb0f34a811b8552745ad809bb0cbf0fa
Oleh Poliakov <106983325+OlehPoliakov@users.noreply.github.com>
2022-12-23 11:49:23 +0200

    add Jimp size avatar 250x250

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,7 +1,6 @@
+const Jimp = require("jimp");
 const path = require('path')
 const fs = require("fs/promises")
-
-
 const {User} = require('../../models/user')
 
 const avatarsDir = path.join(process.cwd(), 'public/avatars');
@@ -9,13 +8,23 @@ const avatarsDir = path.join(process.cwd(), 'public/avatars');
 
 const updateAvatar = async (req, res) => {
   const {_id} = req.user;
+
   const { path: temporaryName, originalname } = req.file;
+
   const fileName = `${_id}_${originalname}`;
+
   const resultUpload = path.join(avatarsDir, fileName)
-  await fs.rename(temporaryName, resultUpload);
+
+  const img = await Jimp.read(temporaryName);
+  await img.resize(250, 250).writeAsync(resultUpload);
+
+  await fs.unlink(req.file.path);
+
   const avatarURL = path.join("avatars", fileName)
-  await User.findByIdAndUpdate(_id, {avatarURL})
-  res.json({ message: 'Avater uploaded successfully', avatarURL});
+
+  const result = await User.findByIdAndUpdate(_id, {avatarURL}, { new: true })
+
+  res.json({ avatarURL: result.avatarURL});
 };
 
 module.exports = updateAvatar;
